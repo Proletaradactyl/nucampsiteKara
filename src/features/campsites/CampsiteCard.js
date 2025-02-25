9a45871cb96156108c7320fd47b4008ce154ac5e
Proletaradactyl <140135988+Proletaradactyl@users.noreply.github.com>
2025-02-25 00:28:31 -0600

    CampsiteDetailPage and useParams

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -2,16 +2,19 @@ import { Card } from "reactstrap";
 import { CardImg } from "reactstrap";
 import { CardImgOverlay } from "reactstrap";
 import { CardTitle } from "reactstrap";
+import { Link } from 'react-router-dom';
 
 const CampsiteCard = ({ campsite }) => {
-    const {image, name} = campsite;
+    const {id, image, name} = campsite;
     return (
+        <Link to={'${id}'}>
         <Card>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
             </CardImgOverlay>
         </Card>
+        </Link>
     )
 }
 
