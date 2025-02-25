b9a5d99053592557dfcbdf2fbb6ce4b9a29af360
Proletaradactyl <140135988+Proletaradactyl@users.noreply.github.com>
2025-02-25 00:02:09 -0600

    Selected camptsite added. Router Setup.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
-
 
 function App() {
-  return (
-    <div className="App">
-            <Navbar dark color='primary' sticky='top' expand='md'>
-                <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            I'm ready for workshop!
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
