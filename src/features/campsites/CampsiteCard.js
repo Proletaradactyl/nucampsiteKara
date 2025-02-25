33550e38dd8a886d533aee553110027154c33b58
Proletaradactyl <140135988+Proletaradactyl@users.noreply.github.com>
2025-02-25 01:00:33 -0600

    Comments

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 const CampsiteCard = ({ campsite }) => {
     const {id, image, name} = campsite;
     return (
-        <Link to={'${id}'}>
+        <Link to={`${id}`}>
         <Card>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
