c974fa08fc2bfdc7ee6e79078db37cf2c9d6e8e8
Proletaradactyl <140135988+Proletaradactyl@users.noreply.github.com>
2025-03-08 01:09:24 -0600

    Updated comment form modal

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,8 +1,20 @@
 import { useState } from "react";
-import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { Formik, Field, Form } from 'formik';
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
+    const handleSubmit = (values) => {
+        const comment ={
+        id: values.id,
+        campsiteId: parseInt(campsiteId),
+        rating: parseInt(values.rating),
+        text: values.commentText,
+        author: values.author
+    };
+    console.log(comment);
+    setModalOpen(false);
+};
 
     return (
         <>
@@ -11,7 +23,57 @@ const CommentForm = ({ campsiteId }) => {
             </Button>
             <Modal isOpen={modalOpen}>
                 <ModalHeader>Add Comment</ModalHeader>
-                <ModalBody>campsite: {campsiteId}</ModalBody>
+                <ModalBody>
+                    <Formik initialValues = {{
+                        rating: undefined,
+                        author: '',
+                        commentText: ''
+                    }} onSubmit={handleSubmit}>
+                        <Form>
+                            <FormGroup>
+                                <Label htmlFor='rating'>
+                                    Rating
+                                </Label>
+                                <Field
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Field>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='author'>
+                                    Author
+                                </Label>
+                                <Field
+                                    name='author'
+                                    placeholder='Your Name'
+                                    className='form-control'
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='commentText'>
+                                    Comment
+                                </Label>
+                                <Field
+                                    name='commentText'
+                                    as='textarea'
+                                    rows='12'
+                                    className='form-control'
+                                />
+                            </FormGroup>
+                            <Button type='submit' color='primary'>
+                                Submit
+                            </Button>
+                        </Form>
+                    </Formik>
+                </ModalBody>
             </Modal>
         </>
     );
